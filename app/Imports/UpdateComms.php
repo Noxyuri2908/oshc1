c220cf147ce7765b3780abdd558c551f0fe7a681
thandanhcuong <thandanhcuong@gmail.com>
2021-09-28 11:22:55 +0700

    update code

--- app/Imports/UpdateComms.php
+++ app/Imports/UpdateComms.php
@@ -16,6 +16,11 @@ class UpdateComms implements ToCollection
         //
         foreach ($rows as $row)
         {
+            if (empty($row[0]))
+            {
+                return 'Done!';
+            }
+
             $apply_id = DB::table('applies')->select('id')->where('ref_no', $row[0])->first()->id ?? '';
             DB::table('hoahongs')
                 ->where('apply_id', $apply_id)
