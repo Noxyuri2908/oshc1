c8a360ea07623d052c31bca8e3f4c535e2eead71
thandanhcuong <thandanhcuong@gmail.com>
2021-10-12 14:33:04 +0700

    update code

--- resources/views/CRM/template_invoice/script_export_to_pdf.blade.php
+++ resources/views/CRM/template_invoice/script_export_to_pdf.blade.php
@@ -1,12 +1,14 @@
-<script>
-    var opt = {
-        filename:     'myfile.pdf',
-        image:        { type: 'jpeg', quality: 0.98 },
-        html2canvas:  { scale: 2 },
-        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-    };
-    $('.btn').on('click', function (){
-        var element = document.getElementById('page');
-        html2pdf().set(opt).from(element).save();
-    })
-</script>
+@push('scripts')
+    <script>
+        var opt = {
+            filename:     'myfile.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+        $('.btn').on('click', function (){
+            var element = document.getElementById('page');
+            html2pdf().set(opt).from(element).save();
+        })
+    </script>
+@endpush
