b79e2c30cf6943dbe9430ddc12f90f3743a51b9e
thandanhcuong <thandanhcuong@gmail.com>
2021-10-15 11:26:55 +0700

    update code

--- app/Imports/CommImport.php
+++ app/Imports/CommImport.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Validators\ValidationException;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class CommImport implements ToCollection
@@ -33,18 +34,28 @@ class CommImport implements ToCollection
                     'payment_note_provider' => getKeyConfigByValue(config('myconfig.payment_note_provider'), $row[24]),
                     'note' => $row[26],
                     'admin_create' => DB::table('admins')->select('id')->where('admin_id', $row[27])->first()->id ?? '',
-                    'created_at' => !empty($row[28]) ? convert_date_to_db(Carbon::parse(Date::excelToDateTimeObject($row[28]))->format('d/m/Y')) : null
+                    'created_at' => !empty($row[28]) && $this->validateDate($row[28], 'd/m/Y')  ? convert_date_to_db(Carbon::parse(Date::excelToDateTimeObject($row[28]))->format('d/m/Y')) : null
                 ]);
-            }catch(\Exception $e)
-            {
-                echo $row;
-                echo $e->getMessage() . ' ===== ';
-                echo $e->getLine() . ' ===== ';
-                echo $e->getTrace() . ' ===== ';
-                return;
+            }catch (\ValidationException $e){
+                $failures = $e->failures();
+
+                foreach ($failures as $failure) {
+                    $failure->row(); // row that went wrong
+                    $failure->attribute(); // either heading key (if using heading row concern) or column index
+                    $failure->errors(); // Actual error messages from Laravel validator
+                    $failure->values(); // The values of the row that has failed.
+                }
             }
         }
 
         return 'Done!';
+    }
+
+
+    function validateDate($date, $format = 'Y-m-d')
+    {
+        $d = \DateTime::createFromFormat($format, $date);
+        // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
+        return $d && $d->format($format) === $date;
     }
 }
