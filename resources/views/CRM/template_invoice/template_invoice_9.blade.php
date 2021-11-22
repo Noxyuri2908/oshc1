47293fe188bb8304ab1c08041416f9f475cc1350
thandanhcuong <thandanhcuong@gmail.com>
2021-11-22 17:20:06 +0700

    update code

--- resources/views/CRM/template_invoice/template_invoice_9.blade.php
+++ resources/views/CRM/template_invoice/template_invoice_9.blade.php
@@ -66,22 +66,22 @@
             <tfoot>
             <tr style=" border-bottom: 9px solid white;">
                 <td style="width: 40%; text-align: left">
-                    <b style="text-align: left; font-size: 10px;margin-bottom: 2px; color: black">{{$dataInvoice['company_name']}}</b>
-                    <p style="text-align: left; font-size: 10px;margin-bottom: 2px">{{$dataInvoice['company_address']}}</p>
-                    <p style="text-align: left; font-size: 10px;margin-bottom: 2px">{{$dataInvoice['company_phone']}}</p>
+                    <b style="text-align: left; font-size: 11px;margin-bottom: 2px; color: black">{{$dataInvoice['company_name']}}</b>
+                    <p style="text-align: left; font-size: 11px;margin-bottom: 2px">{{$dataInvoice['company_address']}}</p>
+                    <p style="text-align: left; font-size: 11px;margin-bottom: 2px">{{$dataInvoice['company_phone']}}</p>
                 </td>
                 <td style="">
-                    <p style="text-align: left; font-size: 9px; margin-bottom: 10px; font-weight: bolder;padding-right: 31px;">Billing address :
-                        <span style="font-weight: normal; font-size: 9px">GLOBAL ONE VISA-HCM 20 Nguyen Thi Minh Khai, Da Kao Ward, District 1, HCMC</span>
+                    <p style="text-align: left; font-size: 10px; margin-bottom: 10px; font-weight: bolder;padding-right: 31px;">BILLING ADDRESS :
+                        <span style="font-weight: normal; font-size: 10px">GLOBAL ONE VISA-HCM 20 Nguyen Thi Minh Khai, Da Kao Ward, District 1, HCMC</span>
                     </p>
-                    <p style="text-align: left; font-size: 9px; margin-bottom: 2px; font-weight: bolder">Invoice No :
-                        <span style="font-weight: normal; font-size: 9px">{{ $dataInvoice['ref_no']}}</span>
+                    <p style="text-align: left; font-size: 10px; margin-bottom: 2px; font-weight: bolder">Invoice No :
+                        <span style="font-weight: normal; font-size: 10px">{{ $dataInvoice['ref_no']}}</span>
                     </p>
-                    <p style="text-align: left; font-size: 9px; margin-bottom: 2px; font-weight: bolder">Date :
-                        <span style="font-weight: normal; font-size: 9px;padding-left: 25px">{{ convert_date_form_db($dataInvoice['date'])}}</span>
+                    <p style="text-align: left; font-size: 10px; margin-bottom: 2px; font-weight: bolder">Date :
+                        <span style="font-weight: normal; font-size: 10px;padding-left: 25px">{{ convert_date_form_db($dataInvoice['date'])}}</span>
                     </p>
-                    <p style="text-align: left; font-size: 9px; margin-bottom: 2px; font-weight: bolder">Term :
-                        <span style="font-weight: normal; font-size: 9px;padding-left: 23px">Immediate Payment</span>
+                    <p style="text-align: left; font-size: 10px; margin-bottom: 2px; font-weight: bolder">Term :
+                        <span style="font-weight: normal; font-size: 10px;padding-left: 25px">Immediate Payment</span>
                     </p>
                 </td>
 
@@ -90,7 +90,7 @@
                 <td style="width: 40%;">
                 </td>
                 <td style="width: 20%;text-align: left">
-                    <span style="color: black; font-size: 10px;margin: 0; ">Reference : </span>
+                    <span style="color: black; font-size: 11px;margin: 0; ">Reference : </span>
                     <span style="color: black;font-size: 12px; ">{{$dataInvoice['cusName']}}</span>
                 </td>
             </tr>
@@ -106,23 +106,23 @@
         <table id="table-2" cellspacing="0" style="width: 100%;">
 
             <tr id="th-header">
-                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 9px;padding: 10px">SERVICE</th>
-                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 9px;padding: 10px">PROVIDER</th>
-                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 9px;padding: 10px">POLICY</th>
-                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 9px;padding: 10px">START DATE</th>
-                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 9px;padding: 10px">END DATE</th>
-                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 9px;padding: 10px;width: 88px">AMOUNT</th>
+                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 10px;padding: 10px">SERVICE</th>
+                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 10px;padding: 10px">PROVIDER</th>
+                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 10px;padding: 10px">POLICY</th>
+                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 10px;padding: 10px">START DATE</th>
+                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 10px;padding: 10px">END DATE</th>
+                <th style="text-align:left; background-color: rgb(234,235,237); text-transform: uppercase; font-size: 10px;padding: 10px;width: 88px">AMOUNT</th>
             </tr>
             <tr>
-                <td style=" text-align:left; font-size: 9px; padding: 5px 15px">{{$dataInvoice['service']}}</td>
-                <td style=" text-align:left; font-size: 9px; padding: 5px 15px">
-                    <p style="font-size: 9px;">{{$dataInvoice['provider_name']}}</p>
-                    <p style="font-size: 9px;">{{ !empty($dataInvoice['cover']) ? '('.$dataInvoice['cover'].')' : '' }}</p>
+                <td style=" text-align:left; font-size: 10px; padding: 5px 15px">{{$dataInvoice['service']}}</td>
+                <td style=" text-align:left; font-size: 10px; padding: 5px 15px">
+                    <p style="font-size: 10px;">{{$dataInvoice['provider_name']}}</p>
+                    <p style="font-size: 10px;">{{ !empty($dataInvoice['cover']) ? '('.$dataInvoice['cover'].')' : '' }}</p>
                 </td>
-                <td style=" text-align:left; font-size: 9px; padding: 5px 9px">{{$dataInvoice['policy']}}</td>
-                <td style=" text-align:left; font-size: 9px; padding: 5px 9px">{{convert_date_form_db($dataInvoice['start_date'])}}</td>
-                <td style=" text-align:left; font-size: 9px; padding: 5px 9px">{{convert_date_form_db($dataInvoice['end_date'])}}</td>
-                <td style=" text-align:left; font-size: 9px; padding: 5px 9px">{{convert_price_float($dataInvoice['amount'])}} {{$dataInvoice['currency']}}</td>
+                <td style=" text-align:left; font-size: 10px; padding: 5px 9px">{{$dataInvoice['policy']}}</td>
+                <td style=" text-align:left; font-size: 10px; padding: 5px 9px">{{convert_date_form_db($dataInvoice['start_date'])}}</td>
+                <td style=" text-align:left; font-size: 10px; padding: 5px 9px">{{convert_date_form_db($dataInvoice['end_date'])}}</td>
+                <td style=" text-align:left; font-size: 10px; padding: 5px 9px">{{convert_price_float($dataInvoice['amount'])}} {{$dataInvoice['currency']}}</td>
             </tr>
             <tr >
                 <td colspan="6" style="padding-left: 0 !important;padding-right: 0 !important;"><hr style="border-style: solid; border-width: 1px; color: #5c5c5c; margin: 0;"></td>
@@ -131,17 +131,17 @@
                 <th></th>
                 <th></th>
                 <th></th>
-                <th colspan="2"  style="color: black; text-align:center; font-size: 9px; padding: 5px 0px; background-color: #D3D3D3" >
-                    <p style="color: black; text-align:left; font-size: 9px; padding: 2px 15px; margin-bottom: 0;letter-spacing: 0.5px" >TOTAL AMOUNT PAYABLE</p>
-                    <p style="color: black; text-align:left; font-size: 9px; padding: 2px 15px; margin-bottom: 0" >(GST inclusive)</p>
+                <th colspan="2"  style="color: black; text-align:center; font-size: 10px; padding: 5px 0px; background-color: #D3D3D3" >
+                    <p style="color: black; text-align:left; font-size: 10px; padding: 5px 15px; margin-bottom: 0;letter-spacing: 0.5px" >TOTAL AMOUNT PAYABLE</p>
+                    <p style="color: black; text-align:left; font-size: 10px; padding: 5px 15px; margin-bottom: 0" >(GST inclusive)</p>
                 </th>
-                <th style=" color: black;text-align: left; padding: 5px 9px; font-size: 9px;background-color: #D3D3D3">{{convert_price_float($dataInvoice['amount'])}} {{$dataInvoice['currency']}}</th>
+                <th style=" color: black;text-align: left; padding: 5px 9px; font-size: 10px;background-color: #D3D3D3">{{convert_price_float($dataInvoice['amount'])}} {{$dataInvoice['currency']}}</th>
             </tr>
             <tr>
                 <td></td>
                 <td colspan="5" style="    text-align: right;padding-right: 0; padding-top: 5px !important;">
-                    <i style="font-size: 8px; color: black;padding-left: 39px;text-align: right;">Note: If you hold a student dependent visa, you must be insured under the same policy as the main</i><br>
-                    <i style="font-size: 8px; color: black; padding-left: 91px;text-align: right;">student visa holder. You are only eligible to hold a single policy if you are the primary visa holder.</i>
+                    <i style="font-size: 9px; color: black;padding-left: 39px;text-align: right;">Note: If you hold a student dependent visa, you must be insured under the same policy as the main</i><br>
+                    <i style="font-size: 9px; color: black; padding-left: 136px;text-align: right;">student visa holder. You are only eligible to hold a single policy if you are the primary visa holder.</i>
                 </td>
             </tr>
         </table>
