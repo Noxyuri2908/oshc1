3f329c3fa770324fca943800bc488168ff635696
thandanhcuong <thandanhcuong@gmail.com>
2021-12-06 14:05:36 +0700

    update code

--- FILES/source/template/template_invoice_9.php
+++ FILES/source/template/template_invoice_9.php
@@ -15,6 +15,11 @@
     h1,h2{margin: 0;}
     p{margin: 1px 0px; color: #222; font-size: 14px}
 
+    #mainContent{
+        width: 806px;
+        margin: auto !important;
+    }
+
     tfoot>tr>td:nth-child(1)>p,
     tfoot>tr>td:nth-child(1)>b{
         text-align: left;
@@ -92,7 +97,7 @@
     /*}*/
     page{
         width: 100%;
-        float: left;
+        padding: 0px 80px;
         height: 1000px;
     }
 
@@ -113,100 +118,102 @@
         font-size: 10px;
     }
 </style>
-<page id="page" backcolor="#fff" backimgx="center" backimgy="bottom" backimgw="100%" backtop="42px" backleft="16px" backright="16px" backbottom="42px" footer="page" style="background: #fff;">
-    <table cellspacing="0" style="width: 100%">
-        <tbody style=" ">
-        <tr>
-            <td rowspan="4" style="width: 20%; padding-top: 45px;">
-                <img style="width:185px;float: left" src="_logo" alt="Logo" id="img">
-            </td>
-            <td rowspan="4" style="width: 22%;vertical-align:bottom; padding-bottom: 10px;">
-                <b  style="font-size: 22px;color: #000">INVOICE</b>
-            </td>
-        </tr>
-        </tbody>
+<div class="content mt-0 mt-lg-3 mt-md-3" id="mainContent">
+    <page id="page" backcolor="#fff" backimgx="center" backimgy="bottom" backimgw="100%" backtop="42px" backleft="16px" backright="16px" backbottom="42px" footer="page" style="background: #fff;">
+        <table cellspacing="0" style="width: 100%">
+            <tbody style=" ">
+            <tr>
+                <td rowspan="4" style="width: 20%; padding-top: 45px;">
+                    <img style="width:185px;float: left" src="_logo" alt="Logo" id="img">
+                </td>
+                <td rowspan="4" style="width: 22%;vertical-align:bottom; padding-bottom: 10px;text-align:right;">
+                    <b  style="font-size: 22px;color: #000">INVOICE</b>
+                </td>
+            </tr>
+            </tbody>
 
-        <tfoot>
-        <tr style=" border-bottom: 9px solid white;">
-            <td style="width: 40%; text-align: left">
-                <b>_company_name</b>
-                <p>_company_address</p>
-                <p>_company_phone</p>
-            </td>
-            <td style="padding-right: 31px">
-                <p>BILLING ADDRESS:
-                    <span>_agentName</span><br>
-                    <span>_agentAddress</span>
-                </p>
-                <p>Invoice No: _ref_no</p>
-                <p>Date: _date</p>
-                <p>Term: Immediate Payment</p>
-            </td>
-        </tr>
-        <tr style="color: #000">
-            <td style="width: 40%;"></td>
-            <td style="width: 20%;text-align: left">
-                <p style="font-size: 11px;margin: 0; ">REFERENCE</p>
-                <b style="font-size: 14px; font-weight: bolder">_cusName</b>
-            </td>
-        </tr>
-        </tfoot>
-    </table>
-    <br>
-    <div class="header" style="display: flex;justify-content: center;align-items: center">
-        <div class="content-right" style="flex: 1">
+            <tfoot>
+            <tr style=" border-bottom: 9px solid white;">
+                <td style="width: 40%; text-align: left">
+                    <b>_company_name</b>
+                    <p>_company_address</p>
+                    <p>_company_phone</p>
+                </td>
+                <td style="padding-right: 31px">
+                    <p>BILLING ADDRESS:
+                        <span>_agentName</span><br>
+                        <span>_agentAddress</span>
+                    </p>
+                    <p>Invoice No: _ref_no</p>
+                    <p>Date: _date</p>
+                    <p>Term: Immediate Payment</p>
+                </td>
+            </tr>
+            <tr style="color: #000">
+                <td style="width: 40%;"></td>
+                <td style="width: 20%;text-align: left">
+                    <p style="font-size: 11px;margin: 0; ">REFERENCE</p>
+                    <b style="font-size: 14px; font-weight: bolder">_cusName</b>
+                </td>
+            </tr>
+            </tfoot>
+        </table>
+        <br>
+        <div class="header" style="display: flex;justify-content: center;align-items: center">
+            <div class="content-right" style="flex: 1">
 
+            </div>
         </div>
-    </div>
 
-    <table id="table-2" cellspacing="0" style="width: 100%; padding-right : 100px">
-        <tr id="th-header">
-            <th style="">SERVICE</th>
-            <th >PROVIDER</th>
-            <th >POLICY</th>
-            <th >START DATE</th>
-            <th >END DATE</th>
-            <th >AMOUNT</th>
-        </tr>
-        <tr id="td-content">
-            <td >_service</td>
-            <td >
-                <p style="font-size: 10px;margin-bottom: 5px;">_provider_name</p>
-                <p style="font-size: 10px;">_cover</p>
-            </td>
-            <td >_policy</td>
-            <td >_startdate</td>
-            <td >_enddate</td>
-            <td >_amount _currency</td>
-        </tr>
-        <tr >
-            <td colspan="6" style="padding: 0 !important;">
-                <hr style="border-style: solid; border-width: 1px; color: #5c5c5c; margin: 0;">
-            </td>
-        </tr>
-        <tr style=" text-align:center;">
-            <th></th>
-            <th></th>
-            <th></th>
-            <th colspan="2" id="total-rate">
-                <p >TOTAL AMOUNT PAYABLE</p>
-                <p >(GST inclusive)</p>
-            </th>
-            <th id="total-rate" >_amount _currency</th>
-        </tr>
-        <tr>
-            <td colspan="5" style=" text-align: right;padding-right: 0; padding-top: 5px !important;">
-                <i style="font-size: 8px; color: black;padding-left: 39px;text-align: right;">Note: If you hold a student dependent visa, you must be insured under the same policy as the main</i><br>
-                <i style="font-size: 8px; color: black; padding-left: 136px;text-align: right;">student visa holder. You are only eligible to hold a single policy if you are the primary visa holder.</i>
-            </td>
-            <td></td>
-        </tr>
-    </table>
-    <br />
-    <div id="more-imf">
-        _content
-    </div>
+        <table id="table-2" cellspacing="0" style="width: 100%; padding-right : 100px">
+            <tr id="th-header">
+                <th style="">SERVICE</th>
+                <th >PROVIDER</th>
+                <th >POLICY</th>
+                <th >START DATE</th>
+                <th >END DATE</th>
+                <th >AMOUNT</th>
+            </tr>
+            <tr id="td-content">
+                <td >_service</td>
+                <td >
+                    <p style="font-size: 10px;margin-bottom: 5px;">_provider_name</p>
+                    <p style="font-size: 10px;">_cover</p>
+                </td>
+                <td >_policy</td>
+                <td >_startdate</td>
+                <td >_enddate</td>
+                <td >_amount _currency</td>
+            </tr>
+            <tr >
+                <td colspan="6" style="padding: 0 !important;">
+                    <hr style="border-style: solid; border-width: 1px; color: #5c5c5c; margin: 0;">
+                </td>
+            </tr>
+            <tr style=" text-align:center;">
+                <th></th>
+                <th></th>
+                <th></th>
+                <th colspan="2" id="total-rate">
+                    <p >TOTAL AMOUNT PAYABLE</p>
+                    <p >(GST inclusive)</p>
+                </th>
+                <th id="total-rate" >_amount _currency</th>
+            </tr>
+            <tr>
+                <td colspan="5" style=" text-align: right;padding-right: 0; padding-top: 5px !important;">
+                    <i style="font-size: 8px; color: black;padding-left: 39px;text-align: right;">Note: If you hold a student dependent visa, you must be insured under the same policy as the main</i><br>
+                    <i style="font-size: 8px; color: black; padding-left: 136px;text-align: right;">student visa holder. You are only eligible to hold a single policy if you are the primary visa holder.</i>
+                </td>
+                <td></td>
+            </tr>
+        </table>
+        <br />
+        <div id="more-imf">
+            _content
+        </div>
 
-</page>
+    </page>
+</div>
 </body>
 </html>
