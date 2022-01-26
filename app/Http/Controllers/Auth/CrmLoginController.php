e7c9de11f8480396086fa72e4bf5b1b204551f65
thandanhcuong <thandanhcuong@gmail.com>
2022-01-26 09:55:18 +0700

    update code

--- app/Http/Controllers/Auth/CrmLoginController.php
+++ app/Http/Controllers/Auth/CrmLoginController.php
@@ -62,12 +62,8 @@ class CrmLoginController extends Controller
 
     function test()
     {
-//        $file = Storage::disk('local')->get('settings/style-setting.txt');
-////        $contents = file_get_contents(asset('backend_CRM/pages/assets/css/style-setting.css'));
-//        $contentsReplace = str_replace('{color}', 'black !important', $file);
-//        file_put_contents('backend_CRM/pages/assets/css/style-setting.css', $contentsReplace);
-        echo 'test';
-
-//        Storage::disk('local')->append('settings/style-setting.txt', $content);
+        $file = Storage::disk('local')->get('settings/style-setting.txt');
+        $contents = 'Anthony /n';
+        Storage::disk('local')->append('settings/style-setting.txt', $contents);
     }
 }
