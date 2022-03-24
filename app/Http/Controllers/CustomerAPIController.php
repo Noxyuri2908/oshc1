d20810eca9df39c6d2d22fa9ca02ea65fc6f1e2f
thandanhcuong <thandanhcuong@gmail.com>
2022-03-24 10:05:13 +0700

    update code

--- app/Http/Controllers/CustomerAPIController.php
+++ app/Http/Controllers/CustomerAPIController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin\HospitalAccess;
 use App\Admin\Service;
 use App\User;
 use Carbon\Carbon;
@@ -59,6 +60,7 @@ class CustomerAPIController extends Controller
         {
             $applies['type_visa'] = 1;
             $applies['type_service'] = 2;
+            $applies['service_country'] = 'A';
             $applies['note'] =
                 'Where is the student studying (instituation)* : ' .$input['witss'] .PHP_EOL .
                 'My current or future location in Australia (campus)* : ' .$input['my']. PHP_EOL .
@@ -70,6 +72,7 @@ class CustomerAPIController extends Controller
         }else if ($input['type'] == 'ovhc'){
             $applies['type_service'] = 3;
             $applies['type_visa'] = $input['type_visa'];
+            $applies['service_country'] = 'A';
             $applies['note'] =
                 'Street address : ' .$input['sa']. ' \r\n   ' .
                 'City / Suburb : ' .$input['cs']. PHP_EOL .
@@ -78,6 +81,7 @@ class CustomerAPIController extends Controller
 
         }else if ($input['type'] == 'usa_si'){
             $applies['type_service'] = 4;
+            $applies['service_country'] = 'U';
             $applies['note'] =
                 'Student/Scholar Status : ' .$input['sss']. PHP_EOL .
                 'Name of school or organization : ' .$input['nosoo']. PHP_EOL .
@@ -85,6 +89,7 @@ class CustomerAPIController extends Controller
 
         }else if ($input['type'] == 'usa_vi'){
             $applies['type_service'] = 6;
+            $applies['service_country'] = 'U';
             $applies['note'] =
                 'Coverage area : ' .$input['ca']. PHP_EOL .
                 'Overall maximum coverage : ' .$input['omc']. PHP_EOL .
@@ -98,15 +103,15 @@ class CustomerAPIController extends Controller
             $applies['status'] = 8;
         }else{
             // param default
-            $applies['service_country'] = 'A';
-            $applies['type_service'] = 2;
+//            $applies['type_service'] = 2;
             $applies['type_invoice'] = 1;
             $applies['status'] = 8;
-            $applies['created_at'] = convert_date_to_db(date('d-m-Y'));
         }
 
         // param default
         $applies['type_get_data_payment'] = 1;
+        $applies['hospital_id'] = HospitalAccess::getIdByName($input['hospital_access']);
+        $applies['created_at'] = convert_date_to_db(date('d-m-Y'));
 
         return $applies;
     }
@@ -126,9 +131,8 @@ class CustomerAPIController extends Controller
             'email' => $input['email'],
             'phone' => $input['phone'],
             'student_id' => $input['student_id'],
-            's_live_in_AS' => $input['s_live_in_AS']
-
-        );
+            's_live_in_AS' =>  getKeyConfigByValue(config('myconfig.live_in_AS'), $input['s_live_in_AS'])
+    );
         $customers['destination'] = 'AU';
         if ($input['type'] == 'ovhc' || $input['type'] == 'si' || $input['type'] == 'vi' || $input['type'] == 'nz_si' || $input['type'] == 'nz_vi'){
             $cover_id = DB::table('covers')->select('id')->where('cover', $input['cover'])->first();
