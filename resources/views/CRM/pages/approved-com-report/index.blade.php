c7fb63c75405abde7615544c57bde5b28d011a7e
Than Danh Cuong <thandanhcuong@Than-Danh-Cuong.local>
2022-05-12 10:44:05 +0700

    update action fields

--- resources/views/CRM/pages/approved-com-report/index.blade.php
+++ resources/views/CRM/pages/approved-com-report/index.blade.php
@@ -10,21 +10,19 @@
 @stop
 @section('content')
     <div class="card">
-        <div class="card-body">
+        <div class="card-body " id="top-filter">
             <div class="row">
                 <div class="col-md-9">
                     <div class="d-flex">
                         <div class="d-flex flex-column pr-15 width-200" id="agent_select_drop_down">
                             <label for="">Agent</label>
                             <select name="" id="agent_select" class="custom-border">
-                                <option value="">1</option>
-                                <option value="">2</option>
+                                <option value=""></option>
                             </select>
                         </div>
                         <div class="d-flex flex-column pr-15">
                             <label for="">Counsellor</label>
                             <select name="" class="width-120 custom-border custom-h" id="counsellor-by-agent">
-                                <option value=""></option>
                             </select>
                         </div>
                         <div class="d-flex flex-column pr-15">
@@ -38,27 +36,29 @@
                         <div class="d-flex flex-column pr-15">
                             <label for="" style="margin-bottom: 0.3rem !important;"></label>
                             <br>
-                            <input type="submit" value="Apply"
-                                   class="width-75 custom-css-input custom-border custom-h">
+                            <button type="submit"
+                                    class="width-75 custom-css-input custom-border custom-h">Apply
+                            </button>
                         </div>
                         <div class="d-flex flex-column pr-15">
                             <label for="" style="margin-bottom: 0.3rem !important;"></label>
                             <br>
                             <button
-                                class="width-85 custom-css-input custom-border custom-h"><i
+                                class="width-85 custom-css-input custom-border custom-h" id="handle-click-reset"><i
                                     class="fas fa-sync-alt custom-icon"></i>Reset
                             </button>
                         </div>
                         <div class="d-flex flex-column pr-15">
                             <label for="">Export as</label>
-                            <input type="submit" value="Export xlsx"
-                                   class="width-88 custom-css-input custom-border custom-h">
+                            <button type="submit"
+                                    class="width-88 custom-css-input custom-border custom-h">Export xlsx
+                            </button>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-3 d-flex justify-content-center align-items-end ">
                     <div class="text-right">
-                        <input type="submit" value="Send mail" class="custom-css-action-send-mail">
+                        <button type="submit" class="custom-css-action-send-mail">Send mail</button>
                     </div>
                 </div>
             </div>
@@ -109,7 +109,7 @@
                         agent_id: $(this).val()
                     },
                     success: function (result) {
-                        var html = '';
+                        var html = '<option value=""></option>';
                         $.each(result.data, function (key, value) {
                             html += `<option value="${value.id}">${value.name}</option>`
                         })
@@ -136,6 +136,12 @@
                         allowInput: true,
                     })
                 }
+            })
+
+            // handle click reset
+            $(document).on('click', '#handle-click-reset', function () {
+                $('#top-filter select').text('');
+                $('#top-filter input').val('');
             })
         })
     </script>
